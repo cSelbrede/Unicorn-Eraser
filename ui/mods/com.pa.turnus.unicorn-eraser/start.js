ef641782bc5d889d1913dec57c19d2f11a811b3e
cSelbrede <christian.selbrede@gmail.com>
2021-02-21 11:55:48 -0600

    Structure start scene mod script according to guard variable design.

--- ui/mods/com.pa.turnus.unicorn-eraser/start.js
+++ ui/mods/com.pa.turnus.unicorn-eraser/start.js
@@ -1,8 +1,15 @@
-(function() {
-    console.log("BYE BYE UNICORNS");
+var unicornEraserLoaded;
 
-    var oldNews = model.processNewsContent;
-    model.processNewsContent = function(html) {
-        return "<p>Hello world!</p>";
+if (!unicornEraserLoaded) {
+    unicornEraserLoaded = true;
+
+    try {
+        var oldNews = model.processNewsContent;
+        model.processNewsContent = function(html) {
+            return "<p>Hello world!</p>";
+        }
+    } catch(e) {
+        console.error(e);
+        console.error(JSON.stringify(e));
     }
-})();
+}
