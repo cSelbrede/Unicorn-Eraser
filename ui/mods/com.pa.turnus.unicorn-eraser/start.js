e542348d31d1e0ba551d652338b08c2da9473df4
cSelbrede <christian.selbrede@gmail.com>
2021-02-21 13:27:23 -0600

    Settle with just removing the whole post if I can't easily filter out the unicorn specific text.

--- ui/mods/com.pa.turnus.unicorn-eraser/start.js
+++ ui/mods/com.pa.turnus.unicorn-eraser/start.js
@@ -4,9 +4,20 @@ if (!unicornEraserLoaded) {
     unicornEraserLoaded = true;
 
     try {
-        var oldNews = model.processNewsContent;
+        var superProcessNewsContent = model.processNewsContent;
         model.processNewsContent = function(html) {
-            return "<p>Hello world!</p>";
+            if (model.newsUrl() === 'https://planetaryannihilation.com/news/rainbows-unicorns-updates/')
+            {
+                return "<p>UNICORN DETECTED...</p><p>ANNIHILATING...</P><p>UNICORN COMMANDER SUCCESSFULLY ANNIHILATED!</p>";
+
+                // var content = superProcessNewsContent(html);
+                // $(content).find('a').hide();
+                // return content;
+            }
+            else
+            {
+                return superProcessNewsContent(html);
+            }
         }
     } catch(e) {
         console.error(e);
