3baaa2506407307882174c09a11c3704f493993c
cSelbrede <christian.selbrede@gmail.com>
2021-02-21 21:02:52 -0600

    Remove variables from global scope.

--- ui/mods/com.pa.turnus.unicorn-eraser/start.js
+++ ui/mods/com.pa.turnus.unicorn-eraser/start.js
@@ -4,23 +4,25 @@ if (!unicornEraserLoaded) {
     unicornEraserLoaded = true;
 
     try {
-        // Switch immediately to news tab, since community videos might have footage of unicorns
-        model.toggleNewsTab();
+        (function(){
+            // Switch immediately to news tab, since community videos might have footage of unicorns
+            model.communityTabGroup('news');
 
-        // Remove unicorn references from the news tab
-        var superProcessNewsContent = model.processNewsContent;
-        model.processNewsContent = function(html) {
-            if (model.newsUrl() === 'https://planetaryannihilation.com/news/rainbows-unicorns-updates/')
-            {
-                var regex = /Rainbows & Unicorns/i
-                trimmedTitle = model.newsTitle().replace(regex, '').trim();
-                model.newsTitle(trimmedTitle);
+            // Remove unicorn references from the news tab
+            var superProcessNewsContent = model.processNewsContent;
+            model.processNewsContent = function(html) {
+                if (model.newsUrl() === 'https://planetaryannihilation.com/news/rainbows-unicorns-updates/')
+                {
+                    var regex = /Rainbows & Unicorns/i
+                    trimmedTitle = model.newsTitle().replace(regex, '').trim();
+                    model.newsTitle(trimmedTitle);
 
-                html = '<p>Full update notes: <a href="https://planetaryannihilation.com/news/rainbows-unicorns-updates/">https://planetaryannihilation.com/news/updates</a>';
+                    html = '<p>Full update notes: <a href="https://planetaryannihilation.com/news/rainbows-unicorns-updates/">https://planetaryannihilation.com/news/updates</a>';
+                }
+
+                return superProcessNewsContent(html);
             }
-
-            return superProcessNewsContent(html);
-        }
+        })();
     } catch(e) {
         console.error(e);
         console.error(JSON.stringify(e));
